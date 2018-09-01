632234306acbc90559174c67d9e89c6a15aaf83e
施哲晨 <sh081042@gmail.com>
2018-09-01 11:33:53 +0800

    add appInfo function

---
+++ src/main/java/com/bean/AppInfo.java
@@ -0,0 +1,52 @@
+package com.bean;
+
+/**
+ * Created by szc on 2018/8/31.
+ */
+public class AppInfo {
+    private int app_id;
+    private String name;
+    private String link;
+    private String icon;
+    private String introduction;
+
+    public int getApp_id() {
+        return app_id;
+    }
+
+    public void setApp_id(int app_id) {
+        this.app_id = app_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getIntroduction() {
+        return introduction;
+    }
+
+    public void setIntroduction(String introduction) {
+        this.introduction = introduction;
+    }
+}
